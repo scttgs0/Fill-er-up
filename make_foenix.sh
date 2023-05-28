332df828c7f9500c88e89ced2e8b8e1ecf78f338
scttgs0 <scttgs0@gmail.com>
2023-05-27 19:43:46 -0500

    remove obsolete files

--- make_foenix.sh
+++
@@ -1,9 +0,0 @@
-
-64tass  --m65816 \
-        --long-address \
-        --flat \
-        --nostart \
-        -o fillerup.pgx \
-        --list=fillerup.lst \
-        --labels=fillerup.lbl \
-        fill-er-up.asm
