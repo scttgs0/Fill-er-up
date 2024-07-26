c851e4bfcafaa31c9acc8779391638478d8b4349
scttgs0 <scttgs0@gmail.com>
2024-07-25 22:15:03 -0500

    updated build script

--- make_f256.sh
+++ make_f256.sh
@@ -6,7 +6,18 @@ mkdir -p obj/
 64tass  --m65c02 \
         --flat \
         --nostart \
+        -D PGX=1 \
         -o obj/fillerup.pgx \
         --list=obj/fillerup.lst \
         --labels=obj/fillerup.lbl \
+        fill-er-up.asm
+
+
+64tass  --m65c02 \
+        --flat \
+        --nostart \
+        -D PGX=0 \
+        -o obj/fillerupB.bin \
+        --list=obj/fillerupB.lst \
+        --labels=obj/fillerupB.lbl \
         fill-er-up.asm
